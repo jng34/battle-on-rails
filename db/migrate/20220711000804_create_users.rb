bab0d5fe30969a422afb0bba43c29195e3abd0d5
William Hwang <willhwang90@gmail.com>
2022-07-11 13:40:14 -0400

    added seeds and password column for users

--- db/migrate/20220711000804_create_users.rb
+++ db/migrate/20220711000804_create_users.rb
@@ -3,8 +3,9 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :profile_img
-      t.integer :base_hp
-      t.integer :base_str
+      t.integer :base_hp, default: 5
+      t.integer :base_str, default: 1
+      t.string :password_digest
 
       t.timestamps
     end
