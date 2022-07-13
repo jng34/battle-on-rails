41b5846e1408f96c976c99cabb1ad9d50d8048d6
JamesNg <njames15@gmail.com>
2022-07-13 17:22:10 -0400

    updated questionscard

--- client/src/QuestionCard.js
+++ client/src/QuestionCard.js
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import questions from './questions.json'
 
+
 function QuestionCard() {
+    const [guess, setGuess] = useState(null)
 
     const questionsArr = questions.questions
+    function randomizer() {
+        return Math.floor(Math.random()*questionsArr.length + 1)
+    }
+
+  
+
     const randomNum = Math.floor(Math.random()*questionsArr.length + 1)
-    console.log(questionsArr)
-    console.log(randomNum)
     const randomQuestion = questionsArr.filter((q) => q.id === randomNum)
-    const prompt = randomQuestion.prompt
-    const answerChoices = randomQuestion.choices
+    const prompt = randomQuestion[0].prompt
+    const answerChoices = randomQuestion[0].choices
+    const correctAns = randomQuestion[0].answer
+    
+
+    function handleSelectAns(e) {
+        setGuess(parseInt(e.target.value))
+    }
+    console.log(guess)
+    
+    function handleAnswerChoice(e) {
+        e.preventDefault();
+        guess === correctAns ? alert('Correct!') : alert('Incorrect.') 
+    }
+    
+    
   return (
         <div>
-            <div className="card">
+            <div className="card" style={{width: "50rem", margin: "auto"}}>
                 <div className="card-header">
                     Question 
                 </div>
                 <div className="card-body">
-                    <p className="card-title">{prompt}</p>
-                    <p className="card-text">{answerChoices}</p>
+                    <p className="card-title text-start">{prompt}</p>
+                    <br />
+                    <div className="text-start">
+                        <form onSubmit={handleAnswerChoice}>
+                            <input type="radio" name='1' value="0" onClick={handleSelectAns}/> &nbsp;
+                            <label htmlFor="answer">{answerChoices[0]}</label><br />
+                            <input type="radio" name='1' value="1" onClick={handleSelectAns}/> &nbsp;
+                            <label htmlFor="answer">{answerChoices[1]}</label><br />
+                            <input type="radio" name='1' value="2" onClick={handleSelectAns}/> &nbsp; 
+                            <label htmlFor="answer">{answerChoices[2]}</label><br />
+                            <input type="radio" name='1' value="3" onClick={handleSelectAns}/> &nbsp; 
+                            <label htmlFor="answer">{answerChoices[3]}</label><br /><br />
+                            <button type="submit" className="btn btn-secondary">Submit Answer</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
